0db8056f80e841adc08e98bf098ba9dad3e2fd3c
Gabriela dos Santos <65128340+gabisinhas@users.noreply.github.com>
2024-10-02 08:49:43 -0300

    commit.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,13 +3,26 @@ import React from 'react'
 const About = () => {
   return (
     <section className='about-container'>
+    <h2>About</h2>
     <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus nulla, cumque delectus voluptatibus vero quam harum voluptas doloribus veritatis ea saepe optio qui nihil dignissimos! Temporibus accusamus illum architecto pariatur.
+    Experienced Full Stack Developer with 3 years of expertise in delivering high-quality, scalable, and maintainable web applications in Finance & Operations, and more than 8 years of experience in Business Analysis/Business Operations. Additionally, I have experience as Project Manager for IT Projects/Processes.
+
+    Specializing in Vue.js for front-end development and Node.js/Python for back-end development, I excel at transforming customer requirements into robust, functional, and aesthetically pleasing UX design frameworks. With a strong foundation in JavaScript, Python, and SQL, I bring a holistic approach to full-stack development, ensuring seamless integration between front-end and back-end components.
+
+    Known for my strong communication skills and adaptability, I am dedicated to continuous professional growth. I thrive in collaborative environments where I leverage my leadership abilities to empower teams, foster open dialogue, and drive collective success.
+    <br/>
+    <b>Currently Studying:</b> React / Typescript / MongoDB / Github Actions / Docker
 
     </p>
 
     <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae repellendus consequatur ipsa ipsam, sed distinctio dicta dolor modi magni, fugiat accusamus temporibus libero quo sit. Repellendus voluptatem impedit labore quasi.
+      <b>Core Competencies:</b>
+
+      Front-End Development: HMTL, CSS, Carbon, Vue.js, JavaScript, LESS/SASS, UX Design, Accessibility
+      Back-End Development: Node.js, Python, SQL, MongoDB, IBM DB2 Relational DB, IBM Cloudant NoSQL
+      Tools & Platforms: GitHub, JIRA, Confluence, SonarQube, Mend, CI/CD pipelines, IBM Cloud, qTest, Docker, Equus APRO
+      Security & Compliance: Code Security, CI/CD, Vulnerabilties management
+      Collaboration & Leadership: Agile Methodologies, Team Empowerment, SCRUM
     </p>
 
     </section>
