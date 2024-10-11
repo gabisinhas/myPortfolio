21aa1e7cd69967459928a8075b552474f988cbd0
Gabriela dos Santos <65128340+gabisinhas@users.noreply.github.com>
2024-10-10 22:27:14 -0300

    commit.

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import '../styles/components/technologies.sass'
 
-const technologies = ["Html", "CSS", "JavaScript","Vue.js","React", "SASS", "Less", "Node.Js", "Python", "CICD", "DevOps", "Code Security", "GitHub", "SQL", "UX Design"]
+const techAreas = ["Html", "CSS", "JavaScript","Vue.js","React", "SASS", "Less", "Node.Js", "Python", "CICD", "DevOps", "Code Security", "GitHub", "SQL", "UX Design"]
 
 const Technologies = () => {
   return (
-    <div className='container-code-tag '>
-      <p className='code-tag'>Gabisinhas</p>
-    </div>
-  )
-}
+  <div className='container-code-tag '>
+  {techAreas.map((area, index) =>(
+      <p className='code-tag' key={index}>{area}</p>      
+  ))
+  }
+  </div>
+  
+)}
 
 export default Technologies
