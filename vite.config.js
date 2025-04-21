36b6ff672bd10d70da018a2e90564b97e0db36ca
Gabriela dos Santos <65128340+gabisinhas@users.noreply.github.com>
2025-04-21 17:46:02 -0300

    new

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './', // Ensure the root is set to the correct directory
   plugins: [react()],
   base: "/myPortfolio/",
   css: {
